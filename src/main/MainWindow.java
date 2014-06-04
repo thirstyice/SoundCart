219c414b8b54da13e1c92e93383b25a470f6f6c4
unknown <297124002@.edu.cbe.ab.ca>
2014-06-04 12:03:01 -0600

    Added some layout - non-functional

--- src/main/MainWindow.java
+++ src/main/MainWindow.java
@@ -3,10 +3,22 @@ package main;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JToolBar;
+import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JScrollPane;
+import javax.swing.JPanel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
 
 public class MainWindow {
 
 	private JFrame frame;
+	static JButton[] button;
 
 	/**
 	 * Launch the application.
@@ -28,16 +40,60 @@ public class MainWindow {
 	 * Create the application.
 	 */
 	public MainWindow() {
-		initialize();
+		initialize(10,10);
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void initialize(int rowsOfButtons, int colsOfButtons) {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(100, 100, 551, 283);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		
+		JToolBar toolBar = new JToolBar();
+		toolBar.setFloatable(false);
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(toolBar, GroupLayout.PREFERRED_SIZE, 242, GroupLayout.PREFERRED_SIZE)
+						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 535, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(toolBar, GroupLayout.PREFERRED_SIZE, 0, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 245, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		
+		JPanel panel = new JPanel();
+		scrollPane.setViewportView(panel);
+		ColumnSpec[] col = new ColumnSpec[colsOfButtons];
+		for (int i=0; i<colsOfButtons; i+=2) {
+			col[i]=FormFactory.RELATED_GAP_COLSPEC;
+			col[i+1]=FormFactory.DEFAULT_COLSPEC;
+		}
+		RowSpec[] row = new RowSpec[rowsOfButtons];
+		for (int i=0; i<rowsOfButtons; i+=2) {
+			row[i]=FormFactory.RELATED_GAP_ROWSPEC;
+			row[i+1]=RowSpec.decode("max(35dlu;default)");
+		}
+		panel.setLayout(new FormLayout(col, row));
+		
+		for (int i=2; i<=colsOfButtons; i+=2) {
+			for (int c=2; c<=rowsOfButtons; c+=2) {
+				JButton btnNewButton = new JButton("Unassigned");
+				panel.add(btnNewButton, i + "," + c + ", default, fill");
+			}
+		};
+		frame.getContentPane().setLayout(groupLayout);
 	}
-
 }
